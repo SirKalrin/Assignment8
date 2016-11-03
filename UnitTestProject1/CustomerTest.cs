fcb95efe7d5fa6afeb8a58e2afb597c7c78a21ec
Greis <mort5240@gmail.com>
2016-11-03 21:10:33 +0100

    Deleted unused branches.

---
+++ UnitTestProject1/CustomerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BE;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void CreateCustomer()
+        {
+            int id = 1;
+            string SSN = "0808921499";
+            int yearBorn = 1992;
+            string name = "Gert Skovmand";
+            DateTime birthDate = DateTime.ParseExact(SSN.Remove(4)+yearBorn, "ddMMyyyy", null);
+            Customer customer = new Customer(id, name) {SSN = SSN, BirthDate = birthDate};
+            
+            Assert.AreEqual(customer.Id, id);
+            Assert.AreEqual(customer.Name, name);
+            Assert.AreEqual(customer.BirthDate, birthDate);
+            Assert.IsTrue(!customer.BankAccounts.Any());
+        }
+    }
+}
